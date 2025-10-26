f8fbfe16ddff4457ac206e79748ba6033c8ec198
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-26 16:41:10 +0700

    Add basic err handler for app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import databaseService from '~/services/database.services'
 import userRouter from '~/routes/users.routes'
 const app = express()
@@ -6,6 +6,13 @@ const port = 3000
 app.use(express.json())
 app.use('/users', userRouter)
 databaseService.connect().catch(console.dir)
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log('Error: ', err)
+  res.status(400).json({
+    err: err.message
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,37 +11,37 @@ userRouter.post('/login', loginValidator, loginController)
  * Method: Post
  * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
  */
-// userRouter.post('/register', registerValidator, registerController)
-userRouter.post(
-  '/register',
-  registerValidator,
-  async (req: Request, res: Response, next: NextFunction) => {
-    // Cách xử lý của version 4
-    try {
-      console.log('Request Handler 1')
-      // next(new Error('Loi roi'))
-      throw new Error('LOI roi')
-    } catch (error) {
-      next(error)
-    }
-    // throw new Error('Loi roi') // express v5.1 : Chỉ cần throw lỗi mặc dù đó là trong hàm async function
-  },
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log('Request Handler 2')
-    next()
-  },
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log('Request Handler 3')
-    res.json({
-      message: 'Ok'
-    })
-  },
-  (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
-    res.status(400).json({
-      err: err.message
-    })
-  }
-)
+userRouter.post('/register', registerValidator, registerController)
+// userRouter.post(
+//   '/register',
+//   registerValidator,
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     // Cách xử lý của version 4
+//     try {
+//       console.log('Request Handler 1')
+//       // next(new Error('Loi roi'))
+//       throw new Error('LOI roi')
+//     } catch (error) {
+//       next(error)
+//     }
+//     // throw new Error('Loi roi') // express v5.1 : Chỉ cần throw lỗi mặc dù đó là trong hàm async function
+//   },
+//   (req: Request, res: Response, next: NextFunction) => {
+//     console.log('Request Handler 2')
+//     next()
+//   },
+//   (req: Request, res: Response, next: NextFunction) => {
+//     console.log('Request Handler 3')
+//     res.json({
+//       message: 'Ok'
+//     })
+//   },
+//   (err: any, req: Request, res: Response, next: NextFunction) => {
+//     console.log(err)
+//     res.status(400).json({
+//       err: err.message
+//     })
+//   }
+// )
 
 export default userRouter
